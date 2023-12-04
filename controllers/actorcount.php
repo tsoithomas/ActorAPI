a235ca33d272a0026fa8a229d949e2d42a377c83
Thomas Tsoi <tsoithomas@outlook.com>
2023-12-04 09:13:26 -0500

    add status codes

--- controllers/actorcount.php
+++ controllers/actorcount.php
@@ -7,8 +7,7 @@ parse_str($query, $get);
 $response = array();
 
 if (!isset($get['city']) && !isset($get['state']) && !isset($get['country'])) {
-    $response["status"] = "error";
-    $response["message"] = "Parameters missing";
+    $response["status"] = "missing_parameters";
 }
 
 if (isset($get["state"])) {
@@ -17,41 +16,53 @@ if (isset($get["state"])) {
     $stmt->execute();
     $result = $stmt->get_result();
     
-    $cnt = 0;
-    $response["count"] = 0;
-    $response["cities"] = array();
-
-    while ($row = $result->fetch_assoc()) {
-        $response["cities"][$row["city"]] = $row["cnt"];
-        $response["count"] += $row["cnt"];
+    if ($result->num_rows > 0) {
+        $cnt = 0;
+        $response["count"] = 0;
+        $response["cities"] = array();
+    
+        while ($row = $result->fetch_assoc()) {
+            $response["cities"][$row["city"]] = $row["cnt"];
+            $response["count"] += $row["cnt"];
+        }
+        $response["status"] = "success";
+    } 
+    else {
+        $response["status"] = "state_not_exist";
     }
 }
-
-if (isset($get["country"])) {
+elseif (isset($get["country"])) {
     $stmt = $mysqli->prepare("SELECT city, `state`, COUNT(*) AS cnt FROM actors, cities WHERE actors.cityid=cities.cityid AND country=? GROUP BY city, `state` ORDER BY `state`, city");
     $stmt->bind_param("s", $get["country"]);
     $stmt->execute();
     $result = $stmt->get_result();
     
-    $count = 0;
-    $curState = "";
-    $stateCount = "";
-    $response["count"] = 0;
-    $response["states"] = array();
-    while ($row = $result->fetch_assoc()) {
-        $city = $row["city"];
-        $state = $row["state"];
-        $cnt = $row["cnt"];
+    if ($result->num_rows > 0) {
+        $count = 0;
+        $curState = "";
+        $stateCount = "";
+        $response["count"] = 0;
+        $response["states"] = array();
 
-        if (!isset($response["states"][$state])) {
-            $response["states"][$state] = array();
-            $response["states"][$state]["count"] = 0;
+        while ($row = $result->fetch_assoc()) {
+            $city = $row["city"];
+            $state = $row["state"];
+            $cnt = $row["cnt"];
+
+            if (!isset($response["states"][$state])) {
+                $response["states"][$state] = array();
+                $response["states"][$state]["count"] = 0;
+            }
+
+            $response["states"][$state]["cities"][$city] = $cnt;
+
+            $response["states"][$state]["count"] += $cnt;
+            $response["count"] += $cnt;
         }
-
-        $response["states"][$state]["cities"][$city] = $cnt;
-
-        $response["states"][$state]["count"] += $cnt;
-        $response["count"] += $cnt;
+        $response["status"] = "success";
+    } 
+    else {
+        $response["status"] = "country_not_exist";
     }
 }
 
