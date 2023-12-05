654ee9f1cd59e1530f15f7f90ae00fb08eb6ae56
Thomas Tsoi <tsoithomas@outlook.com>
2023-12-05 15:51:51 -0500

    prettier

--- index.js
+++ index.js
@@ -4,38 +4,39 @@ async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-
   map = new Map(document.getElementById("map"), {
-    center: { lat: 43.650, lng: -79.395 },
+    center: { lat: 43.65, lng: -79.395 },
     zoom: 4,
     mapId: "DEMO_MAP_ID",
   });
 
+  var infowindow;
+
   for (const loc of locations) {
     const marker = new AdvancedMarkerElement({
-        map: map,
-        position: loc.coords,
-        title: loc.title,
+      map: map,
+      position: loc.coords,
+      title: loc.title,
     });
     const contentString =
-    '<div class="infoWindow">' +
-    '<h1 class="infoTitle">'+loc.title+'</h1>' +
-    '<div class="infoBody"></div>' +
-    "</div>";
-    const infowindow = new google.maps.InfoWindow({
-        content: contentString,
-        ariaLabel: loc.title,
+      '<div class="infoWindow">' +
+      '<h1 class="infoTitle">' +
+      loc.title +
+      "</h1>" +
+      '<div class="infoBody"></div>' +
+      "</div>";
+    infowindow = new google.maps.InfoWindow({
+      content: contentString,
+      ariaLabel: loc.title,
     });
 
     marker.addListener("click", () => {
-        infowindow.open({
-          anchor: marker,
-          map,
-        });
+      infowindow.open({
+        anchor: marker,
+        map,
       });
-
+    });
   }
 }
-
 
 initMap();
