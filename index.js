f572f8add571ecd998c395b45c7d58391a50a7d0
Thomas Tsoi <tsoithomas@outlook.com>
2023-12-08 11:04:43 -0500

    display city name in infowindow

--- index.js
+++ index.js
@@ -1,42 +1,37 @@
 let map;
 
 async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+	const { Map } = await google.maps.importLibrary("maps");
+	const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-  map = new Map(document.getElementById("map"), {
-    center: { lat: 43.65, lng: -79.395 },
-    zoom: 4,
-    mapId: "DEMO_MAP_ID",
-  });
+	map = new Map(document.getElementById("map"), {
+		center: { lat: 43.65, lng: -79.395 },
+		zoom: 4,
+		mapId: "DEMO_MAP_ID",
+	});
 
-  var infowindow;
+  let infowindow = new google.maps.InfoWindow();
+	for (const loc of locations) {
+		const marker = new AdvancedMarkerElement({
+			map: map,
+			position: loc.coords,
+			title: loc.title,
+		});
 
-  for (const loc of locations) {
-    const marker = new AdvancedMarkerElement({
-      map: map,
-      position: loc.coords,
-      title: loc.title,
-    });
-    const contentString =
-      '<div class="infoWindow">' +
-      '<h1 class="infoTitle">' +
-      loc.title +
-      "</h1>" +
-      '<div class="infoBody"></div>' +
-      "</div>";
-    infowindow = new google.maps.InfoWindow({
-      content: contentString,
-      ariaLabel: loc.title,
-    });
 
-    marker.addListener("click", () => {
-      infowindow.open({
-        anchor: marker,
-        map,
-      });
-    });
-  }
+		marker.addListener("click", () => {
+      const contentString =
+			'<div class="infoWindow">' +
+			'<h1 class="infoTitle">' + loc.title +  "</h1>" +
+			'<div class="infoBody">abc</div>' +
+			"</div>";
+      infowindow.setContent(contentString);
+			infowindow.open({
+				anchor: marker,
+				map,
+			});
+		});
+	}
 }
 
 initMap();
--- index.php
+++ index.php
@@ -5,38 +5,38 @@ require_once("./sql.php");
 <html>
 
 <head>
-  <title>Simple Map</title>
-  <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+	<title>Simple Map</title>
+	<script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
 
-  <link rel="stylesheet" type="text/css" href="./style.css" />
-  <script>
-    locations = [<?php
-    $stmt = $mysqli->prepare("SELECT * FROM cities");
-    $stmt->execute();
-    $result = $stmt->get_result();
-    while ($row = $result->fetch_assoc()) {
-      $parts = array();
-      if ($row["city"] != "")
-        array_push($parts, $row["city"]);
-      if ($row["state"] != "")
-        array_push($parts, $row["state"]);
-      if ($row["country"] != "")
-        array_push($parts, $row["country"]);
-      $title = implode(", ", $parts);
+	<link rel="stylesheet" type="text/css" href="./style.css" />
+	<script>
+		locations = [<?php
+		$stmt = $mysqli->prepare("SELECT * FROM cities");
+		$stmt->execute();
+		$result = $stmt->get_result();
+		while ($row = $result->fetch_assoc()) {
+			$parts = array();
+			if ($row["city"] != "")
+				array_push($parts, $row["city"]);
+			if ($row["state"] != "")
+				array_push($parts, $row["state"]);
+			if ($row["country"] != "")
+				array_push($parts, $row["country"]);
+			$title = implode(", ", $parts);
 
-      echo "{coords: {lat:" . $row["lat"] . ", lng:" . $row["lng"] . "}, title:\"$title\"},";
-    }
-    ?>];
-  </script>
-  <script type="module" src="./index.js"></script>
+			echo "{coords: {lat:" . $row["lat"] . ", lng:" . $row["lng"] . "}, title:\"$title\"},";
+		}
+		?>];
+	</script>
+	<script type="module" src="./index.js"></script>
 </head>
 
 <body>
-  <div id="map"></div>
+	<div id="map"></div>
 
-  <!-- prettier-ignore -->
-  <script>(g => { var h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document, b = window; b = b[c] || (b[c] = {}); var d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams, u = () => h || (h = new Promise(async (f, n) => { await (a = m.createElement("script")); e.set("libraries", [...r] + ""); for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]); e.set("callback", c + ".maps." + q); a.src = `https://maps.${c}apis.com/maps/api/js?` + e; d[q] = f; a.onerror = () => h = n(Error(p + " could not load.")); a.nonce = m.querySelector("script[nonce]")?.nonce || ""; m.head.append(a) })); d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)) })
-      ({ key: "<?php echo $_ENV["GOOGLE_API_KEY"]; ?>", v: "weekly" });</script>
+	<!-- prettier-ignore -->
+	<script>(g => { var h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document, b = window; b = b[c] || (b[c] = {}); var d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams, u = () => h || (h = new Promise(async (f, n) => { await (a = m.createElement("script")); e.set("libraries", [...r] + ""); for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]); e.set("callback", c + ".maps." + q); a.src = `https://maps.${c}apis.com/maps/api/js?` + e; d[q] = f; a.onerror = () => h = n(Error(p + " could not load.")); a.nonce = m.querySelector("script[nonce]")?.nonce || ""; m.head.append(a) })); d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)) })
+			({ key: "<?php echo $_ENV["GOOGLE_API_KEY"]; ?>", v: "weekly" });</script>
 </body>
 
 </html>
